dbfe911cade17b13d554fa1a4adec146fe843d12
James Wills <james@wickywills.com>
2016-07-09 19:09:55 +0100

    Action buttons animated

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,21 @@
 jQuery(document).ready(function(){
 
 	resizeDiv();
+
+	// Action buttons
+	jQuery('.section-actions__content .button').on('click', function(e){
+		e.preventDefault();
+
+		// Get the relevant data value for clicked button
+		var animation = jQuery(this).attr('data-animation');
+
+		// Remove any previous classes and add new class for animation
+		jQuery('.section-actions__giovanni').removeClass('jump spin bounce');
+		jQuery('.section-actions__giovanni').addClass(animation);
+		
+
+				
+	});
 	
 });
 
